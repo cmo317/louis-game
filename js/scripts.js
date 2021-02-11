02ac5ad5246a13842d74d8e998a31b79fb203224
cmo317 <78572184+cmo317@users.noreply.github.com>
2021-02-11 18:31:45 -0500

    update html content tag

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log('looks like this is loading okay')
+//functioning hover on/off
+//upper left hover on/off photo
+$(".card").hover( function() {
+	// pull picture URL from data attribute
+	var picUrl = $(this).attr('data-pic')
+  $(this).css('background-image', `url(${picUrl})`)
+},
+//this is the argument for hovering off
+function() {
+  $(this).css('background-image', 'none')
+});
+
+
+
+$("#button").click ( function () {
+var answer1 = parseInt(document.getElementById("box1").value)
+var answer2 = parseInt(document.getElementById("box2").value)
+$("#answer").empty()
+if (((answer1 == 1 ) && (answer2 == 6)) || ((answer1 == 6) && (answer2 == 1))) { $("#answer").text("yes! yay!")
+console.log("yes")}
+else {$("#answer").text("no :( try again")}
+});
