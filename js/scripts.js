2e427965a3b03c8c7a7c907d7ec085811598a843
cmo317 <78572184+cmo317@users.noreply.github.com>
2021-02-11 22:27:45 -0500

    updated gif for no answer

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,13 @@ $("#button").click ( function () {
 var answer1 = parseInt(document.getElementById("box1").value)
 var answer2 = parseInt(document.getElementById("box2").value)
 $("#answer").empty()
+$("#gifbox").css('background-image','none');
 if (((answer1 == 1 ) && (answer2 == 6)) || ((answer1 == 6) && (answer2 == 1))) {
 	$("#answer").text("yes! yay!")
 }
 else if (isNaN(answer1) && isNaN(answer2)) {
 		$("#answer").text("you didn't even try :(")
+		$("#gifbox").css('background-image',`url("https://tv-fanatic-res.cloudinary.com/iu/s--02uNV0Fd--/t_xlarge_l/f_auto,fl_lossy,q_75/v1490231460/attachment/got-shame.gif")`);
 }
 else if (isNaN(answer1) || isNaN(answer2)) {
 		$("#answer").text("try a little harder")
